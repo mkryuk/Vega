ceeca961ffc541a7aa5367f48cab317bc35fa0ff
mkryuk <mkryuk@gmail.com>
2017-05-12 14:06:32 +0300

    section 2 done
    course: https://globallogic.udemy.com/aspnet-core-angular/learn/v4/overview
    
    created api
    /api/makes
    /api/features
    
    created DB and migrations

---
+++ Controllers/MakesController.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System.Linq;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using vega.Controllers.Resources;
+using vega.Models;
+using WebApplicationBasic;
+
+namespace vega.Controllers
+{
+  public class MakesController : Controller
+  {
+    private readonly VegaDbContext dbContext;
+    private readonly IMapper mapper;
+    public MakesController(VegaDbContext dbContext, IMapper mapper)
+    {
+      this.dbContext = dbContext;
+      this.mapper = mapper;
+    }
+    [HttpGet("/api/makes")]
+    public IEnumerable<MakeResource> GetMakes()
+    {
+      var makes = this.dbContext.Makes.Include(m => m.Models).ToList();
+      return this.mapper.Map<List<Make>, List<MakeResource>>(makes);
+    }
+
+  }
+}
---
+++ Migrations/20170505121720_Initial.Designer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using WebApplicationBasic;
+
+namespace Vega.Migrations
+{
+    [DbContext(typeof(VegaDbContext))]
+    [Migration("20170505121720_Initial")]
+    partial class Initial
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.1")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("WebApplicationBasic.Make", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Makes");
+                });
+
+            modelBuilder.Entity("WebApplicationBasic.Model", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int?>("MakeId");
+
+                    b.Property<string>("Name");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("MakeId");
+
+                    b.ToTable("Models");
+                });
+
+            modelBuilder.Entity("WebApplicationBasic.Model", b =>
+                {
+                    b.HasOne("WebApplicationBasic.Make")
+                        .WithMany("Models")
+                        .HasForeignKey("MakeId");
+                });
+        }
+    }
+}
