c280c579760090a7bdde9e2749ce50f59d1566d4
mkryuk <mkryuk@gmail.com>
2017-05-17 19:37:25 +0300

    section 3 done

--- ClientApp/app/app.module.ts
+++ ClientApp/app/app.module.ts
@@ -1,31 +1,43 @@
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { UniversalModule } from 'angular2-universal';
+
 import { AppComponent } from './components/app/app.component'
-import { NavMenuComponent } from './components/navmenu/navmenu.component';
-import { HomeComponent } from './components/home/home.component';
-import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
+import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
+import { HomeComponent } from './components/home/home.component';
+import { FeatureService } from './services/feature.service';
+import { MakeService } from './services/make.service';
+import { NavMenuComponent } from './components/navmenu/navmenu.component';
+import { VehicleFormComponent } from './components/vehicle-form/vehicle-form.component';
 
 @NgModule({
-    bootstrap: [ AppComponent ],
-    declarations: [
-        AppComponent,
-        NavMenuComponent,
-        CounterComponent,
-        FetchDataComponent,
-        HomeComponent
-    ],
-    imports: [
-        UniversalModule, // Must be first import. This automatically imports BrowserModule, HttpModule, and JsonpModule too.
-        RouterModule.forRoot([
-            { path: '', redirectTo: 'home', pathMatch: 'full' },
-            { path: 'home', component: HomeComponent },
-            { path: 'counter', component: CounterComponent },
-            { path: 'fetch-data', component: FetchDataComponent },
-            { path: '**', redirectTo: 'home' }
-        ])
-    ]
+  bootstrap: [AppComponent],
+  declarations: [
+    AppComponent,
+    NavMenuComponent,
+    CounterComponent,
+    FetchDataComponent,
+    HomeComponent,
+    VehicleFormComponent
+  ],
+  imports: [
+    UniversalModule, // Must be first import. This automatically imports BrowserModule, HttpModule, and JsonpModule too.
+    RouterModule.forRoot([
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', component: HomeComponent },
+      { path: 'vehicles/new', component: VehicleFormComponent },
+      { path: 'counter', component: CounterComponent },
+      { path: 'fetch-data', component: FetchDataComponent },
+      { path: '**', redirectTo: 'home' }
+    ]),
+    FormsModule
+  ],
+  providers: [
+    MakeService,
+    FeatureService
+  ]
 })
 export class AppModule {
 }
