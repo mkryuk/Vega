379ec11a1d5a7a69ae66c6a2bd4fc2f0be86c370
mkryuk <mkryuk@gmail.com>
2017-06-09 17:29:25 +0300

    section 8 added photos model and table

--- Persistence/VegaDbContext.cs
+++ Persistence/VegaDbContext.cs
@@ -9,12 +9,14 @@ namespace vega.Persistence
     public DbSet<Model> Models { get; set; }
     public DbSet<Vehicle> Vehicles { get; set; }
     public DbSet<Feature> Features { get; set; }
+    public DbSet<Photo> Photos { get; set; }
     public VegaDbContext(DbContextOptions<VegaDbContext> options)
     : base(options)
     { }
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder) {
-      modelBuilder.Entity<VehicleFeature>().HasKey(vf => new {vf.VehicleId, vf.FeatureId});
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      modelBuilder.Entity<VehicleFeature>().HasKey(vf => new { vf.VehicleId, vf.FeatureId });
     }
 
   }
